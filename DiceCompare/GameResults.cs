bd8ddc44185be35a4951294e5c5243487d5e9420
Aceton05 <michael.koehler@udo.edu>
2021-09-17 16:50:33 +0200

    Add project files.

---
+++ DiceCompare/GameResults.cs
@@ -0,0 +1,6 @@
+﻿namespace DiceCompare
+{
+    internal class GameResults
+    {
+    }
+}
---
+++ DiceCompare/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DiceCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Player> players = SetPlayers(args);
+            List<MatchUps> matchups = CreateMatchups(players);
+            List<GameResults> gameresults = PlayGames(matchups);
+            RankPlayers(gameresults);
+            
+        }
+
+        private static void RankPlayers(List<GameResults> gameresults)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<GameResults> PlayGames(List<MatchUps> matchups)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<MatchUps> CreateMatchups(List<Player> players)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<Player> SetPlayers(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
